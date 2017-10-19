a1c163a7b08f6a978e8c5bb64419d1bf8b7c4886
Your Name <coreyleoc@gmail.com>
2017-10-19 02:28:43 -0700

    feat: add support for data structures

--- src/0-setup/solution.ts
+++ src/0-setup/solution.ts
@@ -1,3 +1,14 @@
-const sum = (a,b) => a + b
+import { Queue } from 'typescript-collections'
+
+const sum = (a: number, b: number) => {
+  const q = new Queue<number>()
+  q.enqueue(a)
+  q.enqueue(b)
+  let sum: number = 0
+  while (!q.isEmpty()) {
+    sum += q.dequeue()
+  }
+  return sum
+}
 
 export { sum }
