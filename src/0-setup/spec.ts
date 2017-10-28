5ac2825fc1dd0b4f8f6bd132830030e1947d0dc9
Corey Cole <coreyleoc@gmail.com>
2017-10-27 22:15:08 -0700

    refactor: test descriptions

--- src/0-setup/spec.ts
+++ src/0-setup/spec.ts
@@ -1,13 +1,15 @@
 import { sum } from './solution'
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
+describe('0) setup', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 
-it('adds 1 + 3 to equal 4', () => {
-  expect(sum(1, 3)).toBe(4)
-})
+  it('adds 1 + 3 to equal 4', () => {
+    expect(sum(1, 3)).toBe(4)
+  })
 
-it('adds 2 + 3 to equal 5', () => {
-  expect(sum(2, 3)).toBe(5)
+  it('adds 2 + 3 to equal 5', () => {
+    expect(sum(2, 3)).toBe(5)
+  })
 })
