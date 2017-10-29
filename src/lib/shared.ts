247f9ecd86a9c635a860c7cfb0ce07e6ccf30ea7
Corey Cole <coreyleoc@gmail.com>
2017-10-28 20:03:36 -0700

    feat: add isPalindrome, isPositive to shared lib

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -1,10 +1,12 @@
 const isNegative = (num: number): boolean => num < 0
 
-const isEven = (num: number) => num % 2 === 0
+const isPositive = (num: number): boolean => num > 0
 
-const isOdd = (num: number) => !isEven(num)
+const isEven = (num: number): boolean => num % 2 === 0
 
-const arrayEquals = (arr1: any[], arr2: any[]) => {
+const isOdd = (num: number): boolean => !isEven(num)
+
+const arrayEquals = (arr1: any[], arr2: any[]): boolean => {
   if (arr1.length !== arr2.length) {
     return false
   }
@@ -17,9 +19,24 @@ const arrayEquals = (arr1: any[], arr2: any[]) => {
   return true
 }
 
+const isPalindrome = (s: string): boolean => {
+  const firstChar = s.charAt(0)
+  const lastChar = s.charAt(s.length - 1)
+  console.log(s, firstChar, lastChar)
+  if (s.length === 1) {
+    return true
+  } else if (firstChar !== lastChar) {
+    return false
+  } else {
+    return s.length === 2 ? true : isPalindrome(s.substring(1, s.length - 1))
+  }
+}
+
 export {
   isNegative,
+  isPositive,
   isEven,
   isOdd,
-  arrayEquals
+  arrayEquals,
+  isPalindrome
 }
